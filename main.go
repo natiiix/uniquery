e539c2b983bcd6b93032167076ab5a3de9a2e9e1
Ivo Meixner <natiwastaken@gmail.com>
2019-11-04 12:28:47 +0100

    Implement new grammar
    The * wildcard specified is not implemented yet.

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	jsonFile  = "test.json"
-	testQuery = "div..href"
+	testQuery = "p..div.1.href"
 )
 
 func must(err error) {
@@ -20,43 +20,50 @@ func must(err error) {
 	}
 }
 
-func find(elem interface{}, queryParts []string) []interface{} {
-	if len(queryParts) == 0 {
-		return []interface{}{elem}
+type Element struct {
+	Value  interface{}
+	Parent *Element
+}
+
+func NewElement(value interface{}, parent *Element) *Element {
+	return &Element{
+		Value:  value,
+		Parent: parent,
+	}
+}
+
+func (e *Element) Query(parts []string) []*Element {
+	// TODO: Implement `*` wildcard specifier support.
+	if len(parts) == 0 {
+		return []*Element{e}
 	}
 
-	specifier := queryParts[0]
-	subquery := queryParts[1:]
+	specifier := parts[0]
+	subquery := parts[1:]
 
-	switch t := elem.(type) {
+	if specifier == "" {
+		return e.Parent.Query(subquery)
+	}
+
+	switch t := e.Value.(type) {
 	case map[string]interface{}:
 		if child, exists := t[specifier]; exists {
-			return find(child, subquery)
+			return NewElement(child, e).Query(subquery)
 		}
 
 	case []interface{}:
-		if specifier == "" {
-			results := []interface{}{}
-
-			for _, item := range t {
-				results = append(results, find(item, subquery)...)
-			}
-
-			return results
-		}
-
 		index, err := strconv.Atoi(specifier)
 		if err != nil || index < 0 || index >= len(t) {
 			log.Fatalln("Invalid index:", specifier)
 		}
 
-		return find(t[index], subquery)
+		return NewElement(t[index], e).Query(subquery)
 
 	default:
 		log.Fatalln("Unexpected JSON type:", t)
 	}
 
-	return []interface{}{}
+	return []*Element{}
 }
 
 func main() {
@@ -68,7 +75,10 @@ func main() {
 	err = json.NewDecoder(f).Decode(&root)
 	must(err)
 
+	rootElem := NewElement(root, nil)
 	queryParts := strings.Split(testQuery, ".")
 
-	fmt.Printf("%#v\n", find(root, queryParts))
+	for i, v := range rootElem.Query(queryParts) {
+		fmt.Printf("%d: %#v\n", i, v)
+	}
 }
