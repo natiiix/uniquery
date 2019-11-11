738501c0de1cc19399e6c89f31c874e8383d182e
Ivo Meixner <natiwastaken@gmail.com>
2019-11-11 10:52:54 +0100

    Improve parser

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"strings"
 )
 
 const (
@@ -25,136 +24,158 @@ type Element struct {
 	Parent *Element
 }
 
-func NewElement(value interface{}, parent *Element) *Element {
-	return &Element{
+func NewElement(value interface{}, parent *Element) Element {
+	return Element{
 		Value:  value,
 		Parent: parent,
 	}
 }
 
-func (e *Element) Query(parts []*QueryPart) []*Element {
+func NewElementRoot(value interface{}) Element {
+	return NewElement(value, nil)
+}
+
+type EqualityFilter struct {
+	Value string
+}
+
+func (f EqualityFilter) IsMatch(value interface{}) bool {
+	if valueStr, ok := value.(string); ok {
+		return valueStr == f.Value
+	}
+
+	return false
+}
+
+type Filter interface {
+	IsMatch(value interface{}) bool
+}
+
+func FilterMatches(f Filter, values []interface{}) []interface{} {
+	filtered := []interface{}{}
+
+	for _, val := range values {
+		if f.IsMatch(val) {
+			filtered = append(filtered, val)
+		}
+	}
+
+	return filtered
+}
+
+func (e Element) Query(parts []QueryPart) []Element {
 	if len(parts) == 0 {
-		return []*Element{e}
+		return []Element{e}
 	}
 
 	part := parts[0]
 	subquery := parts[1:]
 
-	if part.Specifier != nil {
-		spec := *part.Specifier
+	spec := part.Specifier
 
-		if spec == "" {
-			return e.Parent.Query(subquery)
-		}
-
-		switch t := e.Value.(type) {
-		case map[string]interface{}:
-			if spec == "*" {
-				elems := []*Element{}
-
-				for _, v := range t {
-					elems = append(elems, NewElement(v, e).Query(subquery)...)
-				}
-
-				return elems
-			} else if child, exists := t[spec]; exists {
-				return NewElement(child, e).Query(subquery)
-			}
-
-		case []interface{}:
-			if spec == "*" {
-				elems := []*Element{}
-
-				for _, v := range t {
-					elems = append(elems, NewElement(v, e).Query(subquery)...)
-				}
-
-				return elems
-			} else if index, err := strconv.Atoi(spec); err == nil && index >= 0 || index < len(t) {
-				return NewElement(t[index], e).Query(subquery)
-			} else {
-				log.Fatalln("Invalid index:", spec)
-			}
-
-		default:
-			log.Fatalln("Unexpected JSON type:", t)
-		}
+	if spec == "" {
+		return e.Parent.Query(subquery)
 	}
 
-	return []*Element{}
+	switch t := e.Value.(type) {
+	case map[string]interface{}:
+		if spec == "*" {
+			elems := []Element{}
+
+			for _, v := range t {
+				elems = append(elems, NewElement(v, &e).Query(subquery)...)
+			}
+
+			return elems
+		} else if child, exists := t[spec]; exists {
+			return NewElement(child, &e).Query(subquery)
+		}
+
+	case []interface{}:
+		if spec == "*" {
+			elems := []Element{}
+
+			for _, v := range t {
+				elems = append(elems, NewElement(v, &e).Query(subquery)...)
+			}
+
+			return elems
+		} else if index, err := strconv.Atoi(spec); err == nil && index >= 0 || index < len(t) {
+			return NewElement(t[index], &e).Query(subquery)
+		} else {
+			log.Fatalln("Invalid index:", spec)
+		}
+
+	default:
+		log.Fatalln("Unexpected JSON type:", t)
+	}
+
+	return []Element{}
 }
 
 type QueryPart struct {
-	Specifier *string
-	Equality  *string
-	Regex     *string
+	Specifier string
+	Filters   []Filter
 }
 
-func StringOrNil(strPtr *string) string {
-	if strPtr == nil {
-		return fmt.Sprint(strPtr)
-	}
+const (
+	specifierRune = '.'
+	equalityRune  = '='
+)
 
-	return *strPtr
-}
+const (
+	filterEquality = iota
+)
 
-func (qp *QueryPart) String() string {
-	return fmt.Sprintf("%s=%s~%s",
-		StringOrNil(qp.Specifier),
-		StringOrNil(qp.Equality),
-		StringOrNil(qp.Regex))
-}
+func CreateFilter(valueRunes []rune, filterType int) Filter {
+	valueString := string(valueRunes)
 
-func NewQueryPart(specifier, equality *string) *QueryPart {
-	return &QueryPart{
-		Specifier: specifier,
-		Equality:  equality,
-		Regex:     nil,
+	switch filterType {
+	case filterEquality:
+		return EqualityFilter{Value: valueString}
+
+	default:
+		log.Fatalln("Unexpected filter type:", filterType)
+		return nil
 	}
 }
 
-func ParseQuery(query string) []*QueryPart {
+func ParseSinglePart(query []rune) (string, int) {
+	// TODO: Add espacing via backslash and quotes
+	for i, r := range query {
+		if r == specifierRune || r == equalityRune {
+			return string(query[:i]), i
+		}
+	}
+
+	return string(query), len(query)
+}
+
+func ParseQuery(query string) []QueryPart {
 	queryRunes := []rune(query)
-	parts := []*QueryPart{}
-
-	sb := strings.Builder{}
-	escape := false
-
-	var specifier *string = nil
-	var equality *string = nil
-
-	equalityPresent := false
+	parts := []QueryPart{}
 
 	for i := 0; i < len(queryRunes); i++ {
-		r := queryRunes[i]
+		specifier, specifierLength := ParseSinglePart(queryRunes[i:])
+		i += specifierLength
 
-		if escape {
-			sb.WriteRune(r)
-			escape = false
-		} else if r == '\\' {
-			escape = true
-		} else if r == '.' {
-			str := sb.String()
-			sb.Reset()
+		filters := []Filter{}
 
-			if equalityPresent {
-				equality = &str
-				equalityPresent = false
-			} else {
-				specifier = &str
+		for i < len(queryRunes) && queryRunes[i] != specifierRune {
+			filterValue, filterLength := ParseSinglePart(queryRunes[i+1:])
+
+			switch queryRunes[i] {
+			case equalityRune:
+				filters = append(filters, EqualityFilter{Value: filterValue})
+
+			default:
+				log.Fatalln("Unexpected meta rune:", queryRunes[i])
 			}
 
-			parts = append(parts, NewQueryPart(specifier, equality))
-		} else if r == '=' {
-			equalityPresent = true
-			str := sb.String()
-			sb.Reset()
-			specifier = &str
-
-			// TODO: Make sure it wasn't present before
-		} else {
-			sb.WriteRune(r)
+			i += 1 + filterLength
 		}
+
+		parts = append(parts, QueryPart{Specifier: specifier, Filters: filters})
 	}
 
 	return parts
@@ -169,11 +190,10 @@ func main() {
 	err = json.NewDecoder(f).Decode(&root)
 	must(err)
 
-	rootElem := NewElement(root, nil)
 	queryParts := ParseQuery(testQuery)
-
 	fmt.Printf("%+q\n", queryParts)
 
+	rootElem := NewElementRoot(root)
 	for i, v := range rootElem.Query(queryParts) {
 		fmt.Printf("%d: %#v\n", i, v.Value)
 	}
