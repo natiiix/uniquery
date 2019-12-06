7d871a5b2f96a6b7d1d35333e855c0488260567f
Ivo Meixner <natiwastaken@gmail.com>
2019-12-06 13:11:11 +0100

    Implement RunYamlFile func
    This is the YAML equivalent to RunJsonFile.

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 
+	"gopkg.in/yaml.v2"
+
 	"github.com/natiiix/uniquery/pkg/parser"
 )
 
@@ -43,6 +45,21 @@ func RunJsonFile(query string, jsonPath string) ([]Element, error) {
 
 	var root interface{}
 	if err = json.NewDecoder(f).Decode(&root); err != nil {
+		return nil, err
+	}
+
+	return Run(query, root), nil
+}
+
+func RunYamlFile(query string, jsonPath string) ([]Element, error) {
+	f, err := os.Open(jsonPath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var root interface{}
+	if err = yaml.NewDecoder(f).Decode(&root); err != nil {
 		return nil, err
 	}
 
