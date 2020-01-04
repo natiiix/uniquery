ae7b7367d89c7a1a2e0cf9f1203ae762e961468b
Ivo Meixner <natiwastaken@gmail.com>
2020-01-04 22:19:13 +0100

    Update runner tests to work with new result format

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -10,158 +10,164 @@ import (
 
 type testTab []struct {
 	query   string
-	json    string
-	results []interface{}
+	source  string
+	results map[string]interface{}
 }
 
 var testTabJSONChildlessRoot = testTab{
 	// Empty query returns the root element.
-	{``, `"root"`, []interface{}{"root"}},
-	{``, `"1234"`, []interface{}{"1234"}},
+	{``, `"root"`, map[string]interface{}{``: "root"}},
+	{``, `"1234"`, map[string]interface{}{``: "1234"}},
 	// All numbers parsed from JSON are float64 in Golang due to JavaScript's ambiguous Number type.
-	{``, `1234`, []interface{}{1234.0}},
-	{``, `1234.56`, []interface{}{1234.56}},
-	{``, `true`, []interface{}{true}},
-	{``, `null`, []interface{}{nil}},
+	{``, `1234`, map[string]interface{}{``: 1234.0}},
+	{``, `1234.56`, map[string]interface{}{``: 1234.56}},
+	{``, `true`, map[string]interface{}{``: true}},
+	{``, `null`, map[string]interface{}{``: nil}},
 
 	// All of these root elements are childless.
-	{`*`, `"root"`, []interface{}{}},
-	{`*`, `1234.56`, []interface{}{}},
-	{`*`, `true`, []interface{}{}},
-	{`*`, `null`, []interface{}{}},
+	{`*`, `"root"`, map[string]interface{}{}},
+	{`*`, `1234.56`, map[string]interface{}{}},
+	{`*`, `true`, map[string]interface{}{}},
+	{`*`, `null`, map[string]interface{}{}},
 
 	// The wildcard query should return the root element.
-	{`**`, `"root"`, []interface{}{"root"}},
-	{`**`, `1234.56`, []interface{}{1234.56}},
-	{`**`, `true`, []interface{}{true}},
-	{`**`, `null`, []interface{}{nil}},
+	{`**`, `"root"`, map[string]interface{}{``: "root"}},
+	{`**`, `1234.56`, map[string]interface{}{``: 1234.56}},
+	{`**`, `true`, map[string]interface{}{``: true}},
+	{`**`, `null`, map[string]interface{}{``: nil}},
 
 	// There are no children, so there is nothing to return.
-	{`child`, `"root"`, []interface{}{}},
-	{`child`, `1234.56`, []interface{}{}},
-	{`child`, `true`, []interface{}{}},
-	{`child`, `null`, []interface{}{}},
+	{`child`, `"root"`, map[string]interface{}{}},
+	{`child`, `1234.56`, map[string]interface{}{}},
+	{`child`, `true`, map[string]interface{}{}},
+	{`child`, `null`, map[string]interface{}{}},
 
-	{`child.another.abc`, `"root"`, []interface{}{}},
-	{`child.another.abc`, `1234.56`, []interface{}{}},
-	{`child.another.abc`, `true`, []interface{}{}},
+	{`child.another.abc`, `"root"`, map[string]interface{}{}},
+	{`child.another.abc`, `1234.56`, map[string]interface{}{}},
+	{`child.another.abc`, `true`, map[string]interface{}{}},
 
-	{`first.second.0.last`, `"root"`, []interface{}{}},
-	{`first.second.0.last`, `1234.56`, []interface{}{}},
-	{`first.second.0.last`, `true`, []interface{}{}},
+	{`first.second.0.last`, `"root"`, map[string]interface{}{}},
+	{`first.second.0.last`, `1234.56`, map[string]interface{}{}},
+	{`first.second.0.last`, `true`, map[string]interface{}{}},
 
-	{`child.`, `"root"`, []interface{}{}},
-	{`child.`, `1234.56`, []interface{}{}},
-	{`child.`, `true`, []interface{}{}},
+	{`child.`, `"root"`, map[string]interface{}{}},
+	{`child.`, `1234.56`, map[string]interface{}{}},
+	{`child.`, `true`, map[string]interface{}{}},
 
-	{`child..`, `"root"`, []interface{}{}},
-	{`child..`, `1234.56`, []interface{}{}},
-	{`child..`, `true`, []interface{}{}},
+	{`child..`, `"root"`, map[string]interface{}{}},
+	{`child..`, `1234.56`, map[string]interface{}{}},
+	{`child..`, `true`, map[string]interface{}{}},
 
-	{`child........`, `"root"`, []interface{}{}},
-	{`child........`, `1234.56`, []interface{}{}},
-	{`child........`, `true`, []interface{}{}},
+	{`child........`, `"root"`, map[string]interface{}{}},
+	{`child........`, `1234.56`, map[string]interface{}{}},
+	{`child........`, `true`, map[string]interface{}{}},
 
-	{`*.`, `"root"`, []interface{}{}},
-	{`*.`, `1234.56`, []interface{}{}},
-	{`*.`, `true`, []interface{}{}},
+	{`*.`, `"root"`, map[string]interface{}{}},
+	{`*.`, `1234.56`, map[string]interface{}{}},
+	{`*.`, `true`, map[string]interface{}{}},
 
-	{`**.`, `"root"`, []interface{}{}},
-	{`**.`, `1234.56`, []interface{}{}},
-	{`**.`, `true`, []interface{}{}},
+	{`**.`, `"root"`, map[string]interface{}{}},
+	{`**.`, `1234.56`, map[string]interface{}{}},
+	{`**.`, `true`, map[string]interface{}{}},
 }
 
 var testTabJSONSingleChildRoot = testTab{
 	// Empty array and map.
-	{``, `[]`, []interface{}{[]interface{}{}}},
-	{``, `{}`, []interface{}{map[string]interface{}{}}},
+	{``, `[]`, map[string]interface{}{``: []interface{}{}}},
+	{``, `{}`, map[string]interface{}{``: map[string]interface{}{}}},
 
-	{`*`, `[]`, []interface{}{}},
-	{`*`, `{}`, []interface{}{}},
+	{`*`, `[]`, map[string]interface{}{}},
+	{`*`, `{}`, map[string]interface{}{}},
 
-	{`**`, `[]`, []interface{}{[]interface{}{}}},
-	{`**`, `{}`, []interface{}{map[string]interface{}{}}},
+	{`**`, `[]`, map[string]interface{}{``: []interface{}{}}},
+	{`**`, `{}`, map[string]interface{}{``: map[string]interface{}{}}},
 
-	{`child`, `[]`, []interface{}{}},
-	{`child`, `{}`, []interface{}{}},
+	{`child`, `[]`, map[string]interface{}{}},
+	{`child`, `{}`, map[string]interface{}{}},
 
-	{`0`, `[]`, []interface{}{}},
-	{`0`, `{}`, []interface{}{}},
+	{`0`, `[]`, map[string]interface{}{}},
+	{`0`, `{}`, map[string]interface{}{}},
 
 	// Single-item array.
-	{``, `[123]`, []interface{}{[]interface{}{123.0}}},
-	{``, `[123.45]`, []interface{}{[]interface{}{123.45}}},
-	{``, `["abc"]`, []interface{}{[]interface{}{"abc"}}},
-	{``, `[false]`, []interface{}{[]interface{}{false}}},
-	{``, `[null]`, []interface{}{[]interface{}{nil}}},
-	{``, `[[]]`, []interface{}{[]interface{}{[]interface{}{}}}},
-	{``, `[{}]`, []interface{}{[]interface{}{map[string]interface{}{}}}},
+	{``, `[123]`, map[string]interface{}{``: []interface{}{123.0}}},
+	{``, `[123.45]`, map[string]interface{}{``: []interface{}{123.45}}},
+	{``, `["abc"]`, map[string]interface{}{``: []interface{}{"abc"}}},
+	{``, `[false]`, map[string]interface{}{``: []interface{}{false}}},
+	{``, `[null]`, map[string]interface{}{``: []interface{}{nil}}},
+	{``, `[[]]`, map[string]interface{}{``: []interface{}{[]interface{}{}}}},
+	{``, `[{}]`, map[string]interface{}{``: []interface{}{map[string]interface{}{}}}},
 
-	{`*`, `[123]`, []interface{}{123.0}},
-	{`*`, `["abc"]`, []interface{}{"abc"}},
+	{`*`, `[123]`, map[string]interface{}{`0`: 123.0}},
+	{`*`, `["abc"]`, map[string]interface{}{`0`: "abc"}},
 
-	{`**`, `[123]`, []interface{}{[]interface{}{123.0}, 123.0}},
-	{`**`, `["abc"]`, []interface{}{[]interface{}{"abc"}, "abc"}},
+	{`**`, `[123]`, map[string]interface{}{``: []interface{}{123.0}, `0`: 123.0}},
+	{`**`, `["abc"]`, map[string]interface{}{``: []interface{}{"abc"}, `0`: "abc"}},
 
-	{`child`, `[123]`, []interface{}{}},
-	{`child`, `["abc"]`, []interface{}{}},
+	{`child`, `[123]`, map[string]interface{}{}},
+	{`child`, `["abc"]`, map[string]interface{}{}},
 
-	{`0`, `[123]`, []interface{}{123.0}},
-	{`0`, `["abc"]`, []interface{}{"abc"}},
+	{`0`, `[123]`, map[string]interface{}{`0`: 123.0}},
+	{`0`, `["abc"]`, map[string]interface{}{`0`: "abc"}},
 
-	{`1`, `[123]`, []interface{}{}},
-	{`1`, `["abc"]`, []interface{}{}},
+	{`1`, `[123]`, map[string]interface{}{}},
+	{`1`, `["abc"]`, map[string]interface{}{}},
 
-	{`0.`, `[123]`, []interface{}{[]interface{}{123.0}}},
-	{`0.`, `["abc"]`, []interface{}{[]interface{}{"abc"}}},
+	{`0.`, `[123]`, map[string]interface{}{``: []interface{}{123.0}}},
+	{`0.`, `["abc"]`, map[string]interface{}{``: []interface{}{"abc"}}},
 
-	{`0..`, `[123]`, []interface{}{}},
-	{`0..`, `["abc"]`, []interface{}{}},
+	{`0..`, `[123]`, map[string]interface{}{}},
+	{`0..`, `["abc"]`, map[string]interface{}{}},
 
 	// Single-item map.
-	{``, `{"child": 123}`, []interface{}{map[string]interface{}{"child": 123.0}}},
-	{``, `{"child": 123.45}`, []interface{}{map[string]interface{}{"child": 123.45}}},
-	{``, `{"child": "value"}`, []interface{}{map[string]interface{}{"child": "value"}}},
-	{``, `{"child": false}`, []interface{}{map[string]interface{}{"child": false}}},
-	{``, `{"child": null}`, []interface{}{map[string]interface{}{"child": nil}}},
-	{``, `{"child": []}`, []interface{}{map[string]interface{}{"child": []interface{}{}}}},
-	{``, `{"child": {}}`, []interface{}{map[string]interface{}{"child": map[string]interface{}{}}}},
+	{``, `{"child": 123}`, map[string]interface{}{``: map[string]interface{}{"child": 123.0}}},
+	{``, `{"child": 123.45}`, map[string]interface{}{``: map[string]interface{}{"child": 123.45}}},
+	{``, `{"child": "value"}`, map[string]interface{}{``: map[string]interface{}{"child": "value"}}},
+	{``, `{"child": false}`, map[string]interface{}{``: map[string]interface{}{"child": false}}},
+	{``, `{"child": null}`, map[string]interface{}{``: map[string]interface{}{"child": nil}}},
+	{``, `{"child": []}`, map[string]interface{}{``: map[string]interface{}{"child": []interface{}{}}}},
+	{``, `{"child": {}}`, map[string]interface{}{``: map[string]interface{}{"child": map[string]interface{}{}}}},
 
-	{`*`, `{"child": 123}`, []interface{}{123.0}},
-	{`*`, `{"child": "value"}`, []interface{}{"value"}},
+	{`*`, `{"child": 123}`, map[string]interface{}{`"child"`: 123.0}},
+	{`*`, `{"child": "value"}`, map[string]interface{}{`"child"`: "value"}},
 
-	{`*.`, `{"child": 123}`, []interface{}{map[string]interface{}{"child": 123.0}}},
-	{`*.`, `{"child": "value"}`, []interface{}{map[string]interface{}{"child": "value"}}},
+	{`*.`, `{"child": 123}`, map[string]interface{}{``: map[string]interface{}{"child": 123.0}}},
+	{`*.`, `{"child": "value"}`, map[string]interface{}{``: map[string]interface{}{"child": "value"}}},
 
-	{`*..`, `{"child": 123}`, []interface{}{}},
-	{`*..`, `{"child": "value"}`, []interface{}{}},
+	{`*..`, `{"child": 123}`, map[string]interface{}{}},
+	{`*..`, `{"child": "value"}`, map[string]interface{}{}},
 
-	{`**`, `{"child": 123}`, []interface{}{map[string]interface{}{"child": 123.0}, 123.0}},
-	{`**`, `{"child": "value"}`, []interface{}{map[string]interface{}{"child": "value"}, "value"}},
+	{`**`, `{"child": 123}`, map[string]interface{}{``: map[string]interface{}{"child": 123.0}, `"child"`: 123.0}},
+	{`**`, `{"child": "value"}`, map[string]interface{}{``: map[string]interface{}{"child": "value"}, `"child"`: "value"}},
 
-	{`**.`, `{"child": 123}`, []interface{}{map[string]interface{}{"child": 123.0}}},
-	{`**.`, `{"child": "value"}`, []interface{}{map[string]interface{}{"child": "value"}}},
+	{`**.`, `{"child": 123}`, map[string]interface{}{``: map[string]interface{}{"child": 123.0}}},
+	{`**.`, `{"child": "value"}`, map[string]interface{}{``: map[string]interface{}{"child": "value"}}},
 
-	{`**..`, `{"child": 123}`, []interface{}{}},
-	{`**..`, `{"child": "value"}`, []interface{}{}},
+	{`**..`, `{"child": 123}`, map[string]interface{}{}},
+	{`**..`, `{"child": "value"}`, map[string]interface{}{}},
 
-	{`child`, `{"child": 123}`, []interface{}{123.0}},
-	{`child`, `{"child": "value"}`, []interface{}{"value"}},
+	{`child`, `{"child": 123}`, map[string]interface{}{`"child"`: 123.0}},
+	{`child`, `{"child": "value"}`, map[string]interface{}{`"child"`: "value"}},
 
-	{`child.`, `{"child": 123}`, []interface{}{map[string]interface{}{"child": 123.0}}},
-	{`child.`, `{"child": "value"}`, []interface{}{map[string]interface{}{"child": "value"}}},
+	{`child.`, `{"child": 123}`, map[string]interface{}{``: map[string]interface{}{"child": 123.0}}},
+	{`child.`, `{"child": "value"}`, map[string]interface{}{``: map[string]interface{}{"child": "value"}}},
 
-	{`child..`, `{"child": 123}`, []interface{}{}},
-	{`child..`, `{"child": "value"}`, []interface{}{}},
+	{`child..`, `{"child": 123}`, map[string]interface{}{}},
+	{`child..`, `{"child": "value"}`, map[string]interface{}{}},
 
-	{`another`, `{"child": 123}`, []interface{}{}},
-	{`another`, `{"child": "value"}`, []interface{}{}},
+	{`another`, `{"child": 123}`, map[string]interface{}{}},
+	{`another`, `{"child": "value"}`, map[string]interface{}{}},
 
-	{`0`, `{"child": 123}`, []interface{}{}},
-	{`0`, `{"child": "value"}`, []interface{}{}},
+	{`0`, `{"child": 123}`, map[string]interface{}{}},
+	{`0`, `{"child": "value"}`, map[string]interface{}{}},
 
-	{`1`, `{"child": 123}`, []interface{}{}},
-	{`1`, `{"child": "value"}`, []interface{}{}},
+	{`1`, `{"child": 123}`, map[string]interface{}{}},
+	{`1`, `{"child": "value"}`, map[string]interface{}{}},
+
+	{`0`, `{"0": 123}`, map[string]interface{}{`"0"`: 123.0}},
+	{`0`, `{"0": "abc"}`, map[string]interface{}{`"0"`: "abc"}},
+
+	{`1`, `{"0": 123}`, map[string]interface{}{}},
+	{`1`, `{"0": "abc"}`, map[string]interface{}{}},
 }
 
 const complexJSON string = `[
@@ -188,26 +194,26 @@ const complexJSON string = `[
 ]`
 
 var testTabJSONEquality = testTab{
-	{`*.debt=0..name`, complexJSON, []interface{}{"John Daniel", "Robert Denver"}},
-	{`*.debt=1..name`, complexJSON, []interface{}{}},
-	{`*.debt=10..name`, complexJSON, []interface{}{}},
-	{`*.debt=100..name`, complexJSON, []interface{}{}},
-	{`*.debt=1000..name`, complexJSON, []interface{}{"John Doe"}},
+	{`*.debt=0..name`, complexJSON, map[string]interface{}{`2."name"`: "John Daniel", `3."name"`: "Robert Denver"}},
+	{`*.debt=1..name`, complexJSON, map[string]interface{}{}},
+	{`*.debt=10..name`, complexJSON, map[string]interface{}{}},
+	{`*.debt=100..name`, complexJSON, map[string]interface{}{}},
+	{`*.debt=1000..name`, complexJSON, map[string]interface{}{`0."name"`: "John Doe"}},
 }
 
 var testTabJSONEqualityInverted = testTab{
-	{`*.debt!=0..name`, complexJSON, []interface{}{"John Doe", "Jane Doe", "Clark Denver"}},
-	{`*.debt!=1..name`, complexJSON, []interface{}{"John Doe", "Jane Doe", "John Daniel", "Robert Denver", "Clark Denver"}},
+	{`*.debt!=0..name`, complexJSON, map[string]interface{}{`0."name"`: "John Doe", `1."name"`: "Jane Doe", `4."name"`: "Clark Denver"}},
+	{`*.debt!=1..name`, complexJSON, map[string]interface{}{`0."name"`: "John Doe", `1."name"`: "Jane Doe", `2."name"`: "John Daniel", `3."name"`: "Robert Denver", `4."name"`: "Clark Denver"}},
 }
 
 var testTabJSONRegex = testTab{
-	{`*.name~" Doe$"`, complexJSON, []interface{}{"John Doe", "Jane Doe"}},
-	{`*.name~"^John "`, complexJSON, []interface{}{"John Doe", "John Daniel"}},
+	{`*.name~" Doe$"`, complexJSON, map[string]interface{}{`0."name"`: "John Doe", `1."name"`: "Jane Doe"}},
+	{`*.name~"^John "`, complexJSON, map[string]interface{}{`0."name"`: "John Doe", `2."name"`: "John Daniel"}},
 }
 
 var testTabJSONRegexInverted = testTab{
-	{`*.name!~" Doe$"`, complexJSON, []interface{}{"John Daniel", "Robert Denver", "Clark Denver"}},
-	{`*.name!~"^John "`, complexJSON, []interface{}{"Jane Doe", "Robert Denver", "Clark Denver"}},
+	{`*.name!~" Doe$"`, complexJSON, map[string]interface{}{`2."name"`: "John Daniel", `3."name"`: "Robert Denver", `4."name"`: "Clark Denver"}},
+	{`*.name!~"^John "`, complexJSON, map[string]interface{}{`1."name"`: "Jane Doe", `3."name"`: "Robert Denver", `4."name"`: "Clark Denver"}},
 }
 
 const complexYAML = `name: Go
@@ -238,40 +244,39 @@ jobs:
       run: go test -v ./...`
 
 var testTabYAMLGeneral = testTab{
-	{`name`, complexYAML, []interface{}{"Go"}},
-	{`name=Go`, complexYAML, []interface{}{"Go"}},
-	{`name~Go`, complexYAML, []interface{}{"Go"}},
-	{`name~^Go$`, complexYAML, []interface{}{"Go"}},
+	{`name`, complexYAML, map[string]interface{}{`"name"`: "Go"}},
+	{`name=Go`, complexYAML, map[string]interface{}{`"name"`: "Go"}},
+	{`name~Go`, complexYAML, map[string]interface{}{`"name"`: "Go"}},
+	{`name~^Go$`, complexYAML, map[string]interface{}{`"name"`: "Go"}},
 
-	{`on`, complexYAML, []interface{}{[]interface{}{"push", "pull_request"}}},
-	{`on.*~pu`, complexYAML, []interface{}{"push", "pull_request"}},
-	{`on.*~^pu`, complexYAML, []interface{}{"push", "pull_request"}},
-	{`on.*=push`, complexYAML, []interface{}{"push"}},
-	{`on.*~push`, complexYAML, []interface{}{"push"}},
-	{`on.*~^push$`, complexYAML, []interface{}{"push"}},
-	{`on.*=push.`, complexYAML, []interface{}{[]interface{}{"push", "pull_request"}}},
-	{`on.*~push.`, complexYAML, []interface{}{[]interface{}{"push", "pull_request"}}},
-	{`on.*~^push$.`, complexYAML, []interface{}{[]interface{}{"push", "pull_request"}}},
+	{`on`, complexYAML, map[string]interface{}{`true`: []interface{}{"push", "pull_request"}}},
+	{`on.*~pu`, complexYAML, map[string]interface{}{`true.0`: "push", `true.1`: "pull_request"}},
+	{`on.*~^pu`, complexYAML, map[string]interface{}{`true.0`: "push", `true.1`: "pull_request"}},
+	{`on.*=push`, complexYAML, map[string]interface{}{`true.0`: "push"}},
+	{`on.*~push`, complexYAML, map[string]interface{}{`true.0`: "push"}},
+	{`on.*~^push$`, complexYAML, map[string]interface{}{`true.0`: "push"}},
+	{`on.*=push.`, complexYAML, map[string]interface{}{`true`: []interface{}{"push", "pull_request"}}},
+	{`on.*~push.`, complexYAML, map[string]interface{}{`true`: []interface{}{"push", "pull_request"}}},
+	{`on.*~^push$.`, complexYAML, map[string]interface{}{`true`: []interface{}{"push", "pull_request"}}},
 
-	{`jobs.*.steps.*.run..name`, complexYAML, []interface{}{"Get dependencies", "Build", "Run tests"}},
-	{`**.run..name`, complexYAML, []interface{}{"Get dependencies", "Build", "Run tests"}},
+	{`jobs.*.steps.*.run..name`, complexYAML, map[string]interface{}{`"jobs"."build"."steps".2."name"`: "Get dependencies", `"jobs"."build"."steps".3."name"`: "Build", `"jobs"."build"."steps".4."name"`: "Run tests"}},
+	{`**.run..name`, complexYAML, map[string]interface{}{`"jobs"."build"."steps".2."name"`: "Get dependencies", `"jobs"."build"."steps".3."name"`: "Build", `"jobs"."build"."steps".4."name"`: "Run tests"}},
 
-	// TODO: While this is currently the expected result, it is not the desired final result.
-	// Some kind of filtering should happen to remove duplicates.
-	{`on.*~^pu.`, complexYAML, []interface{}{[]interface{}{"push", "pull_request"}, []interface{}{"push", "pull_request"}}},
+	// NOTE: This checks that duplicate results are filtered out.
+	{`on.*~^pu.`, complexYAML, map[string]interface{}{`true`: []interface{}{"push", "pull_request"}}},
 }
 
-func runTests(t *testing.T, tab testTab, verboseName bool, runFunc func(string, string) ([]Element, error)) {
+func runTests(t *testing.T, tab testTab, verboseName bool, runFunc func(string, string) (map[string]Element, error)) {
 	for index, entry := range tab {
 		var testName string
 		if verboseName {
-			testName = fmt.Sprintf("Query:`%s`, JSON:`%s`", entry.query, entry.json)
+			testName = fmt.Sprintf("Query:`%s`, JSON:`%s`", entry.query, entry.source)
 		} else {
 			testName = strconv.Itoa(index)
 		}
 
 		t.Run(testName, func(t *testing.T) {
-			results, err := runFunc(entry.query, entry.json)
+			results, err := runFunc(entry.query, entry.source)
 
 			if err != nil {
 				t.Error(err)
@@ -283,9 +288,19 @@ func runTests(t *testing.T, tab testTab, verboseName bool, runFunc func(string, 
 				return
 			}
 
-			for i, expected := range entry.results {
-				if reality := results[i].Value; !cmp.Equal(reality, expected) {
-					t.Errorf("Unexpected result at index %d: `%#v` (%T) instead of `%#v` (%T)", i, reality, reality, expected, expected)
+			// for k, expected := range entry.results {
+			// 	if reality, exists := results[k]; !exists {
+			// 		t.Errorf("Expected element path `%s` missing from results -- %v", k, results)
+			// 	} else if !cmp.Equal(reality, expected) {
+			// 		t.Errorf("Unexpected value of result with path `%s`: `%#v` (%T) instead of `%#v` (%T)", k, reality, reality, expected, expected)
+			// 	}
+			// }
+
+			for k, expected := range results {
+				if reality, exists := entry.results[k]; !exists {
+					t.Errorf("Expected element path `%s` missing from results -- %v", k, results)
+				} else if !cmp.Equal(reality, expected.Value) {
+					t.Errorf("Unexpected value of result with path `%s`: `%#v` (%T) instead of `%#v` (%T)", k, reality, reality, expected, expected)
 				}
 			}
 		})
